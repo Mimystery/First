898fd5343b3384606eff8337f37c0e7139f43120
Mimystery <vlad1503kudyrko@gmail.com>
2023-03-17 13:12:41 +0200

    patch 1.1

---
+++ MainWindow/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "=>";
+            textBox3.Enabled = false;
+            textBox4.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double kurs1, kurs2, suma, sum_out;
+            kurs1 = double.Parse(textBox1.Text);
+            kurs2 = double.Parse(textBox2.Text);
+            if (radioButton1.Checked == true)
+            {
+                textBox4.Enabled = true;
+               // textBox3.Clear();
+                suma = double.Parse(textBox3.Text);
+                sum_out = suma * kurs1;
+                textBox4.Text = sum_out.ToString();
+            }
+            if (radioButton2.Checked == true)
+            {
+                textBox3.Enabled = true;
+                textBox3.Clear();
+
+                suma = double.Parse(textBox4.Text);
+                sum_out = suma / kurs2;
+                textBox3.Text = sum_out.ToString();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "<=";
+            textBox3.Enabled = true;
+            textBox4.Enabled = false;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
